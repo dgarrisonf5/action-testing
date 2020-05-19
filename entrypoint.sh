fc03e717347a21f23015c910af6ec3bc67372f35
dgarrisonf5 <d.garrison@f5.com>
2020-05-19 16:05:08 -0700

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,8 +1,8 @@
 #!/bin/bash -l
 
-f5 config set-defaults --output json
+f5 config set-defaults --output json --auto-approve
 
-f5 config set-defaults --disable-ssl-warnings true
+f5 config set-defaults --disable-ssl-warnings true --auto-approve
 
 echo ::set-output name=f5-cli-command-output::$($1 2>&1)
 
