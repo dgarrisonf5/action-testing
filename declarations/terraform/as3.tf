f9e3a7c1813b3eb8b09a31af5644c1e52d4652b5
David Garrison <d.garrison@f5.com>
2020-05-21 15:52:30 -0700

    updated

--- declarations/terraform/as3.tf
+++ declarations/terraform/as3.tf
@@ -4,6 +4,13 @@ This Source Code Form is subject to the terms of the Mozilla Public License, v. 
 If a copy of the MPL was not distributed with this file, You can obtain one at https://mozilla.org/MPL/2.0/.
  */
 
+terraform {
+  required_providers {
+    bigip = "=1.1.2"
+  }
+}
+
+
 provider "bigip" {
   address = "b3f01d41-3618-476c-a94f-3a9be92120e9.access.udf.f5.com"
   username = "admin"
@@ -15,5 +22,4 @@ provider "bigip" {
 
 resource "bigip_as3" "as3_example" {
    as3_json = file("as3.json")
-   tenant_name = "bubba"
 }
