ada07c3ac18f11ed5cbbfe087f5f04397e36a967
David Garrison <d.garrison@f5.com>
2020-05-21 13:09:57 -0700

    initial commit

---
+++ declarations/terraform/as3.tf
@@ -0,0 +1,20 @@
+/*
+Copyright 2019 F5 Networks Inc.
+This Source Code Form is subject to the terms of the Mozilla Public License, v. 2.0.
+If a copy of the MPL was not distributed with this file, You can obtain one at https://mozilla.org/MPL/2.0/.
+ */
+
+provider "bigip" {
+  address = "b3f01d41-3618-476c-a94f-3a9be92120e9.access.udf.f5.com"
+  username = "admin"
+  password = "swanlabs1"
+}
+
+
+// config_name is used to set the identity of as3 resource which is unique for resource.
+resource "bigip_as3"  "as3-example1" {
+     as3_json = "${file("as3.json")}" 
+     config_name = "sample_test"
+ }
+
+
