15c29f67a9d6b4d30861ba110a6fb13e0c8d7732
David Garrison <d.garrison@f5.com>
2020-05-22 07:33:43 -0700

    updated

--- declarations/terraform/as3.tf
+++ declarations/terraform/as3.tf
@@ -4,6 +4,11 @@ This Source Code Form is subject to the terms of the Mozilla Public License, v. 
 If a copy of the MPL was not distributed with this file, You can obtain one at https://mozilla.org/MPL/2.0/.
  */
 
+variable "password" {
+  default = "us-east-1"
+}
+
+
 terraform {
   required_providers {
     bigip = "=1.1.1"
@@ -14,8 +19,7 @@ terraform {
 provider "bigip" {
   address = "b3f01d41-3618-476c-a94f-3a9be92120e9.access.udf.f5.com"
   username = "admin"
-  password = "${{ secrets.bigip_password }}"
-}
+  password = var.password
 
 
 // config_name is used to set the identity of as3 resource which is unique for resource.
