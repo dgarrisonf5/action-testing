6e8b4287426fa1eb23c2f95677bf9e846ebbdede
dgarrisonf5 <d.garrison@f5.com>
2020-06-11 11:57:55 -0700

    Update as3.tf

--- declarations/terraform/as3.tf
+++ declarations/terraform/as3.tf
@@ -3,7 +3,7 @@ Copyright 2019 F5 Networks Inc.
 This Source Code Form is subject to the terms of the Mozilla Public License, v. 2.0.
 If a copy of the MPL was not distributed with this file, You can obtain one at https://mozilla.org/MPL/2.0/.
  */
-  
+/*  
 terraform {
   required_providers {
     bigip = "~> 1.2.1"
@@ -15,6 +15,7 @@ provider "bigip" {
   username = "admin"
   password = var.password
 }
+*/
 
 output "file_list" {
   value = var.filelist
@@ -22,8 +23,9 @@ output "file_list" {
 
 
 // config_name is used to set the identity of as3 resource which is unique for resource.
-
+/*
 resource "bigip_as3" "as3_example" {
   as3_json = file("as3.json")
  #config_name = "myconfig"
 }
+*/
